e5b7f22a288b92b1c64d846255568ed05de93d34
D-Semenov <129876525+D-Semenov@users.noreply.github.com>
2023-04-10 00:00:32 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,16 +9,20 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
     import scipy.stats as sts
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    p = (x_success + y_success) / (x_cnt + y_cnt)
-    d = p2 - p1
-    se = (p*(1-p)*(1/x_cnt + 1/y_cnt))**0.5
-    z = d / se
-    alpha = 0.04
-    z_critical = sts.norm.ppf(1 - alpha/2)
-    if abs(z) > z_critical:
-        return True # отклоняем нулевую гипотезу
-    else:
-        return False # не отклоняем нулевую гипоте
+    x = [[x_cnt,x_success],[y_cnt,y_success]]
+    oddsratio, pvalue = sts.fisher_exact(observations)
+    answer = pvalue < 0.04
+    return answer
+    #p1 = x_success / x_cnt
+    #p2 = y_success / y_cnt
+    #p = (x_success + y_success) / (x_cnt + y_cnt)
+    #d = p2 - p1
+    #se = (p*(1-p)*(1/x_cnt + 1/y_cnt))**0.5
+    #z = d / se
+    #alpha = 0.04
+    #z_critical = sts.norm.ppf(1 - alpha/2)
+    #if abs(z) > z_critical:
+        #return True # отклоняем нулевую гипотезу
+    #else:
+        #return False # не отклоняем нулевую гипоте
     #return ... # Ваш ответ, True или False
