92a6bda68f455a9aecb4e9e4e36cf42c9f130d54
D-Semenov <129876525+D-Semenov@users.noreply.github.com>
2023-04-10 14:31:23 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,10 +9,16 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
     import scipy.stats as sts
-    x = [[x_cnt,x_success],[y_cnt,y_success]]
+    #x = [[x_cnt,x_success],[y_cnt,y_success]]
+    x =  [[x_success,x_cnt-x_success],[y_success,y_cnt-y_success]]
     oddsratio, pvalue = sts.fisher_exact(x)
-    answer = pvalue > 0.04
+    #answer = pvalue > 0.04
+    answer = pvalue < 0.04
     return answer
+   
+  
+  
+  
     #p1 = x_success / x_cnt
     #p2 = y_success / y_cnt
     #p = (x_success + y_success) / (x_cnt + y_cnt)
